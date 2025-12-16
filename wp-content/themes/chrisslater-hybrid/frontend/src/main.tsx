1122edafca97b540e2bb4aa1ffce6345ec9879e1
slate22 <136153815+slate22@users.noreply.github.com>
2025-12-16 01:22:38 -0500

    FIX: Restore App Logic + Safe Mode (Removed Apollo/Providers to prevent crash)

--- wp-content/themes/chrisslater-hybrid/frontend/src/main.tsx
+++ wp-content/themes/chrisslater-hybrid/frontend/src/main.tsx
@@ -4,21 +4,14 @@ import App from './App.tsx'
 import './index.css'
 import { HelmetProvider } from 'react-helmet-async'
 import { BrowserRouter } from 'react-router-dom'
-import { ApolloProvider } from '@apollo/client'
-import { client } from './lib/apollo.ts'
 
-import { CartProvider } from './context/CartContext.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <ApolloProvider client={client}>
-            <CartProvider>
-                <HelmetProvider>
-                    <BrowserRouter>
-                        <App />
-                    </BrowserRouter>
-                </HelmetProvider>
-            </CartProvider>
-        </ApolloProvider>
+        <HelmetProvider>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </HelmetProvider>
     </React.StrictMode>,
 )
